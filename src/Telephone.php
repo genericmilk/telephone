938dcdd6d98dac68ed4113b3c4e3b473184c265a
Peter Day <peterday.main@gmail.com>
2019-10-21 15:39:53 +0100

    Post req

--- src/Telephone.php
+++ src/Telephone.php
@@ -32,10 +32,35 @@ class Telephone extends Controller
             $response = json_decode(json_encode($response)); // force convert to php object
             return $response;
         } else {
-            throw new Exception('Telephone dropped call; '.$err);
+            throw new \Exception('Telephone dropped call; '.$err);
         }
     }
-    public static function post($Url,$Headers = [],$Body){
+    public static function post($Url,$Headers = [],$Body = []){
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $Url,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 30,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS => $Body,
+          CURLOPT_HTTPHEADER => $Headers,
+        ));
+        
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        
+        curl_close($curl);
+        
+        if ($err) {
+            $response = json_decode($response);
+            $response = json_decode(json_encode($response)); // force convert to php object
+            return $response;
+        } else {
+            throw new \Exception('Telephone dropped call; '.$err);
+        }
     }
 }
