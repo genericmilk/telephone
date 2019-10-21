25438825c1939a67037c1dfe80ae46fdba948c3f
Peter Day <peterday.main@gmail.com>
2019-10-21 15:24:56 +0100

    First call

--- src/Telephone.php
+++ src/Telephone.php
@@ -9,9 +9,33 @@ use App\Http\Controllers\Controller;
 class Telephone extends Controller
 {
     public static function get($Url,$Headers = []){
-        // throw new \Exception('No Element Configuration Set');
-        return (object)[
-            'telephone' => 'ringring'
-        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $Url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => $Headers
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!$err) {
+            $response = json_decode($response);
+            $response = json_decode(json_encode($response)); // force convert to php object
+            return $response;
+        } else {
+            throw new Exception('Telephone dropped call; '.$err);
+        }
+    }
+    public static function post($Url,$Headers = [],$Body){
+
     }
 }
